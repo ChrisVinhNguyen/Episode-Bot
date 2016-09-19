807b3133fe18931353fbf69e2172a83a9b8b24cc
Chris <chris.vinhnguyen@mail.utoronto.ca>
2016-09-19 17:42:00 -0400

    fooling around with parser, will probably need to use different parser

--- link_finder.py
+++ link_finder.py
@@ -15,11 +15,20 @@ class LinkFinder(HTMLParser):
         if tag == 'a':
             for (attribute, value) in attrs:
                 if attribute == 'href':
+                    text = self.get_starttag_text()
                     url = parse.urljoin(self.base_url, value)
+                    print(text)
+                    print(url)
                     self.links.add(url)
+
+    #def handle_data(self, data):
+     #   if data == '[HorribleSubs] D.Gray-man Hallow - 12 [480p].mkv':
+      #      print("Encountered some data  :", data)
 
     def page_links(self):
         return self.links
 
     def error(self, message):
         pass
+    
+   
