9081aa2fb14a370a2343c4043d16cfc65b00263d
ChrisVinhNguyen <chris.314159@hotmail.com>
2016-09-18 22:39:00 -0400

    added spider code from newboston tutorial

--- spider.py
+++ spider.py
@@ -2,13 +2,37 @@ from urllib.request import urlopen
 from link_finder import LinkFinder
 from misc import *
 
-class Spider:
 
-    #class variables
+class Spider:
+    # class variables
     project_name = ''
     base_url = ''
     domain_name = ''
     queue_file = ''
     crawled_file = ''
+    queue = set()
+    crawled = set()
 
-    
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.queue_file = Spider.project_name + '/crawled.txt'
+        self.boot()
+        self.crawl_page('First spider', Spider.base_url)
+
+    @staticmethod
+    def boot():
+        create_project_dir(Spider.project_name)
+        create_data_files(Spider.project_name, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set( Spider.crawled_file)
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + ' now crawling ' + page_url)
+            print('Queue ' + str(len(Spider.queue)) + ' | Crawled '+ str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_link(page_url))
+            Spider.queue.remove(page_url)
