396c0e8e00b4b9c17ea7e3c9efb2145ff2f55240
ChrisVinhNguyen <chris.314159@hotmail.com>
2016-10-02 13:19:30 -0400

    added scheduler

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from downloader import *
 from searcher import *
+import schedule
+import time
 
 URL = 'http://www.nyaa.se/'
 TEST= '[HorribleSubs] Active Raid S2 - 12 [720p].mkv'
 TESTLINK = 'http://www.nyaa.se/?page=download&tid=855452'
 TESTFILE= 'downloads/qwrqwsrqw.torrent'
 
-print('Booting up...')
-html = getHTML(URL)
-search('season_conf/watch_list',html)
-print('Shutting down...')
+print('Start')
+
+def job():
+    print('---------------------------------------------------------------------------------------------------------------')
+    print('Booting up...')
+    html = getHTML(URL)
+    search('season_conf/watch_list', html)
+    print('Shutting down...')
+
+schedule.every(3).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    #time.sleep(1)
+
