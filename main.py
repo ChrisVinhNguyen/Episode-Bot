36f488aa6ea349992beda4d864b64d036f275b12
ChrisVinhNguyen <chris.314159@hotmail.com>
2016-10-03 12:57:51 -0400

    changed back test url

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import schedule
 import time
 
 
-#URL = 'http://www.nyaa.se'
-URL = 'http://www.nyaa.se/?page=search&cats=0_0&filter=0&term=WWW.wor'
+URL = 'http://www.nyaa.se'
+#URL = 'http://www.nyaa.se/?page=search&cats=0_0&filter=0&term=WWW.wor'
 TEST= '[HorribleSubs] Active Raid S2 - 12 [720p].mkv'
 TESTLINK = 'http://www.nyaa.se/?page=download&tid=855918'
 TESTFILE= 'E:\Projects\Episode-Bot\downloads\[HorribleSubs] Mobile Suit Gundam - Iron-Blooded Orphans S2 - 01 [1080p].mkv.torrent'
